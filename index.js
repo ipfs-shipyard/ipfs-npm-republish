6c1b023a84fc741531149ead8d9961402f11a184
Andrew Nesbitt <andrewnez@gmail.com>
2019-05-28 15:14:36 +0100

    Add support for republishing individual modules

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var os = require('os');
-var { exec } = require('child_process');
 var republish = require('./lib/republish');
 
 // setup
 var registry = 'https://registry.npmjs.org/'
 var localIPFSgateway = 'http://localhost:8080/'
-var tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'ipfs-npm-republish-'));
-// should probably empty tmp dir first
 
-// if no args
+var args = process.argv.slice(2)
 
-republish.republishDependenciesFromDisk(registry, tmpDir, localIPFSgateway)
-
-// otherwise first arg is package name
-
-// download packument for package name
-// add
+if (args.length === 0){
+  republish.republishDependenciesFromDisk(registry, localIPFSgateway)
+} else {
+  republish.republishPackage(args[0], registry, localIPFSgateway)
+}
